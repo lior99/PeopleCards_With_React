f9e5411ace70f12fb8dd328ac38e91c8d92d46ca
Lior Shtivelman <lior.shtivelman@gmail.com>
2016-12-07 19:00:49 +0200

    added SASS

---
+++ src/components/EmployeeCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class EmployeeCard extends React.Component {
+	render() {
+		let employee = this.props.employee;
+
+		return (
+					<div>
+						<div className="employee-name">{ employee.name }</div>
+						<div className="company">{ employee.company }</div>
+						<div className="email">{ employee.email }</div>		
+					</div>
+		);
+	}
+}
+
+export default EmployeeCard;
---
+++ src/services/CardsService.js
@@ -0,0 +1,59 @@
+const CardsService = {
+	getEmployees:function() {
+		// with 3 seconds delay
+		// const url = `http://www.filltext.com/?rows=200&id={index}&firstName={firstName}&lastName={lastName}&company={business}&email={email}&pretty=true&delay=3`;
+
+		// without delay		
+		const url = `http://www.filltext.com/?rows=200&id={index}&firstName={firstName}&lastName={lastName}&company={business}&email={email}&pretty=true`;
+		return new Promise((resolve, reject) => {
+			fetch(url) // => using fetch api
+				.then((response) => {
+					response
+						.json()
+						.then(data => {
+								let convertedResponse = data.map(this.createEmployeeObject);
+								resolve(convertedResponse);			
+						 });
+				})
+				.catch((err) => {
+					reject(err);
+				});
+		});
+	},
+
+
+	createEmployeeObject: function (rawPersonObject) {
+		return {
+			name : `${ rawPersonObject.firstName } ${ rawPersonObject.lastName }`,
+			id : rawPersonObject.id,
+			firstName : rawPersonObject.firstName,
+			lastName : rawPersonObject.lastName,
+			company : rawPersonObject.company,
+			email : rawPersonObject.email
+		}
+	},
+
+	filterEmployees:function (arr, filter, possibleKeys) {
+		if (filter === '') {
+			return arr;
+		}
+		else {
+			let lowerCaseFilter = filter.toLowerCase();
+			let filteredArray = arr.filter((item) => {
+				let storedItem = null;
+				for(let key of possibleKeys) {
+					if (item[key].toLowerCase().includes(lowerCaseFilter)) {
+						storedItem = item;
+						break;
+					}
+				}
+				return storedItem;
+			})
+
+			return filteredArray;
+		}
+	}
+
+}
+
+export default CardsService;
